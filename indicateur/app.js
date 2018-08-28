99f8a29f8296914dc182a882cbc10241cfe74bbd
nassim ouaret <n.ouaret91@gmail.com>
2018-08-28 10:33:21 +0200

    creation du projet sous visual studio

---
+++ indicateur/app.js
@@ -0,0 +1,124 @@
+﻿'use strict';
+var debug = require('debug');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+app.set('port', process.env.PORT || 3000);
+
+server.listen(app.get('port'), function () {
+    debug('Express server listening on port ' + server.address().port);
+});
+
+var listDesinducateur = new Array();
+
+io.on('connection', function (socket) {
+    console.log("connexion ok - ID attribué au client" + socket.id)
+    if (listDesinducateur.length > 0) {
+        listDesinducateur.forEach((element) => {
+            io.emit('cree', { nom: element.nom })       
+        });
+    }
+
+    socket.on('creer', function (data) {
+        var nvID = {
+            nom: data.nom,
+            bit_1: { adr: data.bit_1, value: "" },
+            bit_2: { adr: data.bit_1 + 1, value: "" },
+            bit_3: { adr: data.bit_1 + 2, value: "" },
+            bit_4: { adr: data.bit_1 + 3, value: "" },
+            bit_5: { adr: data.bit_1 + 4, value: "" },
+            bit_6: { adr: data.bit_1 + 5, value: "" },
+            text: ""
+        }
+        listDesinducateur.push(nvID)
+        socket.emit('cree', { nom: data.nom });
+    });
+});
+
+
+// communication API
+var modbus = require("modbus-stream");
+
+modbus.tcp.connect(502, "192.168.0.25", { debug: ""/*"automaton-2454"*/ }, (err, connection) => {
+    // do something with connection
+    setInterval(() => {
+        if (listDesinducateur.length > 0) {
+            listDesinducateur.forEach((element) => {
+                connection.readCoils({ address: element.bit_1.adr, quantity: 6 }, (err, res) => {
+
+                    console.log("data - " + res.response);
+                    element.bit_1.value = res.response.data[0];
+                    element.bit_2.value = res.response.data[1];
+                    element.bit_3.value = res.response.data[2];
+                    element.bit_4.value = res.response.data[3];
+                    element.bit_5.value = res.response.data[4];
+                    element.bit_6.value = res.response.data[5];
+
+                    element.text = res.response.data[5].toString() + res.response.data[4].toString() + res.response.data[3].toString()
+                        + res.response.data[2].toString() + res.response.data[1].toString() + res.response.data[0].toString();
+
+                    //broadcast value
+                    io.emit('mja', { nom: element.nom, valeur: element.text });
+                })
+            });
+        } 
+    }, 100)
+});
---
+++ indicateur/public/javascripts/client.js
@@ -0,0 +1,111 @@
+﻿var socket = io.connect('http://localhost:1337');
+
+
+function creerID() {
+    var _nom = document.getElementById("nom").value;
+    var _bit_1 = document.getElementById("bit_1").value;
+
+    if (_nom != "" && _bit_1 != "") {
+        socket.emit('creer', {
+            nom: _nom,
+            bit_1: _bit_1,
+        });
+    } else {
+        //afficher un message
+        document.getElementById('msg').style.display = "block";
+    }    
+}
+
+socket.on('cree', (data) => {
+    var ligne = document.createElement("tr");
+
+    var clnom = document.createElement("td");
+    var clvaleur = document.createElement("td");
+
+    clnom.innerHTML = data.nom
+    clvaleur.id = data.nom
+
+    clnom.classList.add("w3-center");
+    clvaleur.classList.add("w3-center"); clvaleur.classList.add("w3-circle"); clvaleur.classList.add("w3-red");
+
+    ligne.appendChild(clnom);
+    ligne.appendChild(clvaleur);
+
+    document.getElementById('listDesID').appendChild(ligne);    
+});
+
+socket.on('mja', (data) => {
+    document.getElementById(data.nom).innerHTML = valeurID(data.valeur);
+    console.log("hello");
+});
+
+function valeurID(valeurBin) {
+    var valeurText;
+    switch (valeurBin) {
+        case '000000': valeurText = "";         break;// 0
+        case '000001': valeurText = "ZD";       break;// 1
+        case '000010': valeurText = "Q1";       break;// 2
+        case '000011': valeurText = "Q2";       break;// 3
+        case '000100': valeurText = "Q3";       break;// 4
+        case '000101': valeurText = "Q4";       break;// 5
+        case '000110': valeurText = "Q5";       break;// 6
+        case '000111': valeurText = "Q6";       break;// 7
+        case '001000': valeurText = "V1";       break;// 8
+        case '001001': valeurText = "V2";       break;// 9
+        case '001010': valeurText = "VA";       break;// 10
+        case '001011': valeurText = "VC";       break;// 11
+        case '001100': valeurText = "VI";       break;// 12
+        case '001101': valeurText = "VG";       break;// 13
+        case '001110': valeurText = "VL";       break;// 14
+        case '001111': valeurText = "VT";       break;// 15
+        case '010000': valeurText = "VR";       break;// 16
+        case '010001': valeurText = "VS";       break;// 17
+        case '010010': valeurText = "VU";       break;// 18
+        case '010011': valeurText = "M";        break;// 19
+        case '010100': valeurText = "ML";       break;// 20
+        case '010101': valeurText = "SS";       break;// 21
+        case '010110': valeurText = "1";        break;// 22
+        case '010111': valeurText = "2";        break;// 23
+        case '011000': valeurText = "3";        break;// 24
+        case '011001': valeurText = "4";        break;// 25
+        case '011010': valeurText = "5";        break;// 26
+        case '011011': valeurText = "6";        break;// 27
+        case '011100': valeurText = "7";        break;// 28
+        case '011101': valeurText = "8";        break;// 29
+        case '011110': valeurText = "9";        break;// 30
+        case '011111': valeurText = "10";       break;// 31
+        case '100000': valeurText = "11";       break;// 32
+        case '100001': valeurText = "12";       break;// 33
+        case '100010': valeurText = "13";       break;// 34
+        case '100011': valeurText = "14";       break;// 35
+        case '100100': valeurText = "15";       break;// 36
+        case '100101': valeurText = "21";       break;// 37
+        case '100110': valeurText = "22";       break;// 38
+        case '100111': valeurText = "23";       break;// 39
+        case '101000': valeurText = "24";       break;// 40
+        case '101001': valeurText = "25";       break;// 41
+        case '101010': valeurText = "26";       break;// 42
+        case '101011': valeurText = "27";       break;// 43
+        case '101100': valeurText = "28";       break;// 44
+        case '101101': valeurText = "29";       break;// 45
+        case '101110': valeurText = "30";       break;// 46
+        case '101111': valeurText = "G1";       break;// 47
+        case '110000': valeurText = "G2";       break;// 48
+        case '110001': valeurText = "G3";       break;// 49
+        case '110010': valeurText = "G4";       break;// 50
+        case '110011': valeurText = "PS";       break;// 51
+        case '110100': valeurText = "GN";       break;// 52
+        case '110101': valeurText = "HM";       break;// 53
+        case '110110': valeurText = "HD";       break;// 54
+        case '110111': valeurText = "RP";       break;// 55
+        case '111000': valeurText = "EP";       break;// 56
+        case '111001': valeurText = "TF";       break;// 57
+        case '111010': valeurText = "2B";       break;// 58
+        case '111011': valeurText = "16";       break;// 59
+        case '111100': valeurText = "18";       break;// 60
+        case '111101': valeurText = "AB";       break;// 61
+        case '111110': valeurText = "";         break;// 62
+        case '111111': valeurText = "";         break;// 63 
+    }
+    return valeurText;
+}
